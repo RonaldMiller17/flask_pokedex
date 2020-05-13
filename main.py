33a147733a63dca9e07fd82a52775c8ed247a07e
Ronald Miller <aurora_secondary@rim.lan>
2020-05-13 13:27:44 -0400

    generate list of 151 Kanto pokemon from json response and return as base.html template, added /index route for testing w/out call due to rate limiting

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # imports
-from flask import Flask
+from flask import Flask, render_template
+from flask_table import Table, Col
 import os
 import requests
 import json
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -12,25 +14,37 @@ def home():
 
     # TODO: use levenschtien for fuzzy search??
 
+    pokemon_list = []
+
     # reguest Kanto pokedex
     response = requests.get("https://pokeapi.co/api/v2/pokedex/2/")
     kanto_pokemon = response.json()['pokemon_entries']
     for pokemon in kanto_pokemon:
-        print(pokemon)
+        # print(pokemon['pokemon_species']['name'])
+        pokemon_list.append(pokemon['pokemon_species']['name'])
+
+    print(f"Length - {len(pokemon_list)}")
+    print(pokemon_list)
 
 
     # write json to file
     # with open('kanto_pokemon.txt', 'w') as outfile:
     #     json.dump(response.json()['pokemon_entries'], outfile)
 
+    # TODO: pass results (df) to template that will pull from Results class
+
     # welcome message
-    return "Welcome to the Pokédex!"
+    return render_template('base.html', len = len(pokemon_list), Pokemon = pokemon_list)
 
 
 # testing another page
 @app.route("/index")
 def index():
-    return "This is a different page"
+    # making list of pokemons 
+    pokemon =["Pikachu", "Charizard", "Squirtle", "Jigglypuff",  
+           "Bulbasaur", "Gengar", "Charmander", "Mew", "Lugia", "Gyarados"] 
+    return render_template('base.html', len = len(Pokemons), Pokemon = pokemon)
+
 
 if __name__ == "__main__":
     app.run(debug=True) 
