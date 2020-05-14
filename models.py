d663b069f70379e49749c51b92020d6fd78d1aab
Ronald Miller <aurora_secondary@RIM.attlocal.net>
2020-05-14 18:18:45 -0400

    added color and description fields and logic to find description in english from results

--- models.py
+++ models.py
@@ -13,12 +13,17 @@ class Pokemon_Model(Base):
     types = Column(String(50))
     capture_rate = Column(String(50))
     shape = Column(String(50))
+    color = Column(String(15))
+    description = Column(String(100))
 
-    def __init__(self, name=None, types=None, capture_rate=None, shape=None):
+    def __init__(self, name=None, types=None, capture_rate=None, shape=None, color=None, description=None):
         self.name = name
         self.types = types
         self.capture_rate = capture_rate
         self.shape = shape
+        self.color = color
+        self.description = description
+
 
 
     def __repr__(self):
