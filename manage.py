a12ab037f857e343bd25c2feed1b6ec339e568d8
Ronald Miller <aurora_secondary@RIM.attlocal.net>
2020-05-15 06:49:18 -0400

    manage.py uses alembic to control db migrations

---
+++ manage.py
@@ -0,0 +1,14 @@
+import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import app, db
+from database import init_db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+manager.add_command('initdb', init_db())
+
+if __name__ == '__main__':
+    manager.run()
