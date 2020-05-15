547f7ab578d0492bc8a4ca4513cab55a5ff7134e
Ronald Miller <aurora_secondary@RIM.attlocal.net>
2020-05-15 08:07:44 -0400

    formatting changes

--- database.py
+++ database.py
@@ -14,6 +14,7 @@ db_session = scoped_session(sessionmaker(autocommit=False,
 Base = declarative_base()
 Base.query = db_session.query_property()
 
+
 def init_db():
     
     from models import Pokemon_Model
