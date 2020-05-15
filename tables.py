06c6a77e3464ec6a88137993428e662fe6159a85
RonaldMiller17 <ronald.miller@me.com>
2020-05-15 07:05:16 -0400

    Delete tables.py

--- tables.py
+++
@@ -1,9 +0,0 @@
-from flask_table import Table, Col
-
-# table keys for results table
-class Results(Table):
-    id = Col('id', show = False)
-    name = Col('Name')
-    pokemon_type = Col('Type')
-    capture_rate = Col('Capture Rate (%)')
-    shape = Col('Shape')
